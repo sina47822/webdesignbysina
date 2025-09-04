e475362a9d577a187b77568f9e3eadf347a19b92
sinaa.afshar@gmail.com <sinaa.afshar@gmail.com>
2025-09-04 12:25:10 +0330

    edit loader with png image

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }>) {
   return (
       <div className="overflow-x-hidden">
-          <Loader text="gnidaoL" />
+          <Loader />
           <ScrollToTopProgress />
           <Header1 />
           {children}

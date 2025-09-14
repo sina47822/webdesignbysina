1a9604ee3cd069feb664cb2a7cd87f2814ab5d9d
sinaa.afshar@gmail.com <sinaa.afshar@gmail.com>
2025-09-14 15:18:51 +0330

    add and edit portfolio pictures

--- src/components/Pages/Portfolios/ScrollStack.tsx
+++ src/components/Pages/Portfolios/ScrollStack.tsx
@@ -6,17 +6,22 @@ import Image from "next/image";
 import ScrollStack, { ScrollStackItem } from "./ScrollStackItem"; // ← دقت کن مسیر همانی باشد
 
 const mock = [
-  { id: 1, text: "طراحی سایت شرکتی" ,title: "سایت ابرنواختر", link: "https://abarnoakhtar.ir/new", image: "/assets/images/works/002 - AbarnoAkhtar - [abarnoakhtar.ir].png" },
-  { id: 2, text: "طراحی سایت شخصی" ,title: "سایت هانی ادیب آزاد", link: "https://haniadibazad.ir", image: "/assets/images/works/003 - - - [haniadibazad.ir].png" },
-  { id: 3, text: "طراحی سایت شخصی" ,title: "سایت دئودار", link: "https://deodar.ir", image: "/assets/images/works/004 - وبسایت ریسپانسیو دئودار - [deodar.ir].png" },
-  { id: 4, text: "طراحی سایت شرکتی" ,title: "سایت شوکا", link: "https://shooka.deodar.ir", image: "/assets/images/works/005 - Shooka — Company Portfolio Website - [shooka.deodar.ir].png" },
-  { id: 5, text: "طراحی سایت شرکتی" ,title: "سایت Asora wise trading", link: "https://firexnull.deodar.ir", image: "/assets/images/works/006 - FireXNull - [firexnull.deodar.ir].png" },
-  { id: 6, text: "طراحی سایت فروشگاهی" ,title: "طراحی سایت فروشگاه پی خاک سنگ", link: "/portfolio/#", image: "/assets/images/works/4.jpg" },
-  { id: 7, text: "طراحی سایت" ,title: "طراحی سایت رزرو آنلاین الوله", link: "/portfolio/#", image: "/assets/images/works/5.jpg" },
-  { id: 8, text: "طراحی سایت" ,title: "طراحی سایت فروشگاه لوازم ورزشی آلپ", link: "/portfolio/#", image: "/assets/images/works/6.jpg" },
-  { id: 9, text: "طراحی سایت" ,title: "طراحی سایت شرکت ایفاکو", link: "/portfolio/#", image: "/assets/images/works/6.jpg" },
-  { id: 10, text: "طراحی سایت" ,title: "طراحی سایت شرکت الکتروایمپورت", link: "/portfolio/#", image: "/assets/images/works/6.jpg" },
-  { id: 11, text: "طراحی سایت" ,title: "طراحی سایت فروشگاه رادین پدیده ایرانیان", link: "/portfolio/#", image: "/assets/images/works/3.jpg" },
+  { id: 1, text: "طراحی سایت شرکتی" ,title: "سایت ابرنواختر", link: "https://abarnoakhtar.ir/new", image: "/assets/images/works/abarnoakhtar.png" },
+  { id: 2, text: "طراحی سایت شخصی" ,title: "سایت هانی ادیب آزاد", link: "https://haniadibazad.ir", image: "/assets/images/works/haniadibazad.png" },
+  { id: 3, text: "طراحی سایت شخصی" ,title: "سایت دئودار", link: "https://deodar.ir", image: "/assets/images/works/deodar.png" },
+  { id: 4, text: "طراحی سایت شرکتی" ,title: "سایت شوکا", link: "https://shooka.deodar.ir", image: "/assets/images/works/shooka-deodar.png" },
+  { id: 5, text: "طراحی سایت شرکتی" ,title: "سایت Asora wise trading", link: "https://firexnull.deodar.ir", image: "/assets/images/works/firexnull-deodar.png" },
+  { id: 6, text: "طراحی سایت فروشگاهی" ,title: "طراحی سایت فروشگاه پی خاک سنگ", link: "/portfolio/#", image: "/assets/images/works/peykhaksang.png" },
+  { id: 7, text: "طراحی سایت فروشگاهی" ,title: "طراحی سایت شرکت الکتروایمپورت ", link: "https://shop.deodar.ir", image: "/assets/images/works/shop-deodar.png" },
+  { id: 9, text: "طراحی سایت فروشگاهی" ,title: "طراحی سایت فروشگاه لوازم ورزشی آلپ", link: "/portfolio/#", image: "/assets/images/works/biakooh.png" },
+  { id: 10, text: "طراحی سایت شرکتی" ,title: "طراحی سایت شرکت ایفاکو", link: "/portfolio/#", image: "/assets/images/works/iphaco.png" },
+  { id: 8, text: "طراحی سایت" ,title: "طراحی سایت رزرو آنلاین الوله", link: "/portfolio/#", image: "/assets/images/works/1.jpg" },
+  { id: 11, text: "طراحی سایت" ,title: "طراحی سایت شرکت سافت میکر", link: "/portfolio/#", image: "/assets/images/works/2.jpg" },
+  { id: 12, text: "طراحی سایت" ,title: "طراحی سایت فروشگاه رادین پدیده ایرانیان", link: "/portfolio/#", image: "/assets/images/works/3.jpg" },
+  { id: 13, text: "طراحی سایت" ,title: "طراحی سایت فروشگاه دیدگاه آرت گالری", link: "/portfolio/#", image: "/assets/images/works/4.jpg" },
+  { id: 14, text: "طراحی سایت" ,title: "طراحی سایت شرکتی هورآسمان ", link: "/portfolio/#", image: "/assets/images/works/5.jpg" },
+  { id: 15, text: "طراحی سایت" ,title: "طراحی سایت شرکتی پوتیران", link: "/portfolio/#", image: "/assets/images/works/6.jpg" },
+  { id: 16, text: "طراحی سایت" ,title: "طراحی سایت خبری کمپیا", link: "/portfolio/#", image: "/assets/images/works/6.jpg" },
 ];
 
 export default function ScrollStacks() {
@@ -35,7 +40,7 @@ export default function ScrollStacks() {
             <div className="relative w-full aspect-[18/9] rounded-4xl overflow-hidden">
               <Image
                 src={item.image}
-                alt={item.text}
+                alt={item.title}
                 fill
                 sizes="(min-width: 1024px) 900px, 100vw"
                 className="
@@ -43,6 +48,7 @@ export default function ScrollStacks() {
                   transform-gpu will-change-transform
                   transition-transform duration-500 ease-out
                   group-hover:scale-[1]
+                  hover-pan
                 "
                 draggable={false}
                 priority={item.id === 1}
